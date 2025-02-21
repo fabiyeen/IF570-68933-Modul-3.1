67c6b06626d0d46c0aaf9056e517cbd1a00e12a2
fabiyeen <163744056+fabiyeen@users.noreply.github.com>
2025-02-21 15:08:41 +0700

    step 5 1

--- app/src/main/java/androidx/compose/samples/crane/home/LandingScreen.kt
+++ app/src/main/java/androidx/compose/samples/crane/home/LandingScreen.kt
@@ -24,14 +24,22 @@ import androidx.compose.samples.crane.R
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.runtime.LaunchedEffect
+import kotlinx.coroutines.delay
 
 private const val SplashWaitTime: Long = 2000
 
 @Composable
 fun LandingScreen(onTimeout: () -> Unit, modifier: Modifier = Modifier) {
     Box(modifier = modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
-        // TODO Codelab: LaunchedEffect and rememberUpdatedState step
-        // TODO: Make LandingScreen disappear after loading data
+        // Start a side effect to load things in the background
+        // and call onTimeout() when finished.
+        // Passing onTimeout as a parameter to LaunchedEffect
+        // is wrong! Don't do this. We'll improve this code in a sec.
+        LaunchedEffect(onTimeout) {
+            delay(SplashWaitTime) // Simulates loading things
+            onTimeout()
+        }
         Image(painterResource(id = R.drawable.ic_crane_drawer), contentDescription = null)
     }
 }
