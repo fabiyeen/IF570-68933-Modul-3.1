bc56560986296b583e39c1a74ea28f92cab61a29
fabiyeen <163744056+fabiyeen@users.noreply.github.com>
2025-02-21 15:03:44 +0700

    advanced state 1

--- app/src/main/java/androidx/compose/samples/crane/home/MainViewModel.kt
+++ app/src/main/java/androidx/compose/samples/crane/home/MainViewModel.kt
@@ -23,6 +23,9 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import javax.inject.Inject
@@ -65,4 +68,9 @@ class MainViewModel @Inject constructor(
             //  _suggestedDestinations.value = newDestinations
         }
     }
+
+    private val _suggestedDestinations = MutableStateFlow<List<ExploreModel>>(emptyList())
+
+    val suggestedDestinations: StateFlow<List<ExploreModel>> = _suggestedDestinations.asStateFlow()
+
 }
