9aacd21115cdae9d915ed5c29e93f6b1612ea6c8
fabiyeen <163744056+fabiyeen@users.noreply.github.com>
2025-02-21 15:21:20 +0700

    step 7 2

--- app/src/main/java/androidx/compose/samples/crane/base/EditableUserInput.kt
+++ app/src/main/java/androidx/compose/samples/crane/base/EditableUserInput.kt
@@ -32,23 +32,30 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.compose.runtime.saveable.Saver
 import androidx.compose.runtime.saveable.listSaver
+import androidx.compose.runtime.saveable.rememberSaveable
 
 class EditableUserInputState(private val hint: String, initialText: String) {
-
     var text by mutableStateOf(initialText)
-        private set
-
-    fun updateText(newText: String) {
-        text = newText
-    }
 
     val isHint: Boolean
         get() = text == hint
+
+    companion object {
+        val Saver: Saver<EditableUserInputState, *> = listSaver(
+            save = { listOf(it.hint, it.text) },
+            restore = {
+                EditableUserInputState(
+                    hint = it[0],
+                    initialText = it[1],
+                )
+            }
+        )
+    }
 }
 
 @Composable
 fun rememberEditableUserInputState(hint: String): EditableUserInputState =
-    remember(hint) {
+    rememberSaveable(hint, saver = EditableUserInputState.Saver) {
         EditableUserInputState(hint, hint)
     }
 
